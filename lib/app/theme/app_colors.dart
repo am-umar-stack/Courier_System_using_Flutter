d68dcf9f95c9a85e8fb75fde94470bdac931510d
am-umar-stack <qaziumarakunz786@gmail.com>
2026-07-20 00:15:12 +0500

    feat(app): configure app setup & theme/routes (app_colors.dart)

---
+++ lib/app/theme/app_colors.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF1A73E8);
+  static const Color primaryLight = Color(0xFF4DA3FF);
+  static const Color primaryDark = Color(0xFF0D47A1);
+  static const Color primaryContainer = Color(0xFFD3E3FD);
+  static const Color onPrimaryContainer = Color(0xFF041E49);
+
+  static const Color secondary = Color(0xFF00BFA5);
+  static const Color secondaryLight = Color(0xFF5DF2D6);
+  static const Color secondaryDark = Color(0xFF008C76);
+  static const Color secondaryContainer = Color(0xFFB2F5EA);
+  static const Color onSecondaryContainer = Color(0xFF003D33);
+
+  static const Color tertiary = Color(0xFFFF6D00);
+  static const Color tertiaryContainer = Color(0xFFFFDDB3);
+  static const Color onTertiaryContainer = Color(0xFF3E1600);
+
+  static const Color backgroundLight = Color(0xFFF8F9FA);
+  static const Color surfaceLight = Color(0xFFFFFFFF);
+  static const Color surfaceVariantLight = Color(0xFFF1F3F4);
+  static const Color onBackgroundLight = Color(0xFF202124);
+  static const Color onSurfaceLight = Color(0xFF202124);
+  static const Color onSurfaceVariantLight = Color(0xFF5F6368);
+  static const Color outlineLight = Color(0xFFDADCE0);
+
+  static const Color backgroundDark = Color(0xFF121212);
+  static const Color surfaceDark = Color(0xFF1E1E1E);
+  static const Color surfaceVariantDark = Color(0xFF2D2D2D);
+  static const Color onBackgroundDark = Color(0xFFE8EAED);
+  static const Color onSurfaceDark = Color(0xFFE8EAED);
+  static const Color onSurfaceVariantDark = Color(0xFF9AA0A6);
+  static const Color outlineDark = Color(0xFF3C4043);
+
+  static const Color success = Color(0xFF0F9D58);
+  static const Color successContainer = Color(0xFFCEEAD6);
+  static const Color error = Color(0xFFD93025);
+  static const Color errorContainer = Color(0xFFFAD2CF);
+  static const Color warning = Color(0xFFF9AB00);
+  static const Color warningContainer = Color(0xFFFEF7E0);
+  static const Color info = Color(0xFF4285F4);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color grey50 = Color(0xFFF8F9FA);
+  static const Color grey100 = Color(0xFFF1F3F4);
+  static const Color grey200 = Color(0xFFE8EAED);
+  static const Color grey300 = Color(0xFFDADCE0);
+  static const Color grey400 = Color(0xFFBDC1C6);
+  static const Color grey500 = Color(0xFF9AA0A6);
+  static const Color grey600 = Color(0xFF80868B);
+  static const Color grey700 = Color(0xFF5F6368);
+  static const Color grey800 = Color(0xFF3C4043);
+  static const Color grey900 = Color(0xFF202124);
+
+  static const Color shimmerBase = Color(0xFFE0E0E0);
+  static const Color shimmerHighlight = Color(0xFFF5F5F5);
+  static const Color shimmerBaseDark = Color(0xFF2D2D2D);
+  static const Color shimmerHighlightDark = Color(0xFF424242);
+
+  static const Color statusPending = Color(0xFFF9AB00);
+  static const Color statusPickedUp = Color(0xFF4285F4);
+  static const Color statusInTransit = Color(0xFF1A73E8);
+  static const Color statusDelivered = Color(0xFF0F9D58);
+  static const Color statusCancelled = Color(0xFFD93025);
+}
